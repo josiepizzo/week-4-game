73c920c47af248828e1923e3f30e9999cd0b1155
judith pizzolato <judith pizzolato>
2016-05-28 13:20:57 -0400

    commit revised html and js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,13 @@
-$(document).ready(funtion(){
+$( document ).ready(function() {
 
-crystals = ['assets/images/diamond-blue.png', 'assets/images/diamond-green.png', 'assets/images/diamond-pink.png', 'assets/images/diamond-yellow.png'];
+crystals = ['assets/images/diamond-blue.png','assets/images/diamond-green.png','assets/images/diamond-pink.png','assets/images/diamond-yellow.png'];
 
 var counter = 0;
 var wins = 0;
 var losses = 0;
+
+var showScore = document.getElementById('totalScore');
+
 $("#win").text(wins);
 $("#loss").text(losses);
 
@@ -15,7 +18,7 @@ function newCrystals(){
 	// code ...
 	var numbers = []
 			while(numbers.length < 4){
-			  var randomnumber = Math.floor((Math.random()*12);
+			  var randomnumber = Math.floor(Math.random()*(12-1)) +1;
 			  var found = false;
 			  for (var i=0; i< numbers.length; i++){
 				if (numbers[i] == randomnumber){
@@ -24,8 +27,8 @@ function newCrystals(){
 			  }
 			  if(!found)numbers[numbers.length]=randomnumber;
 			}
-		console.log(numbers);
-}s
+		console.log( "ready!" );
+}
 
 });
 //funtion newGame(argument){
